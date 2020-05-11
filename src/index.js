38367ca457165c69f95c2532d751b4b8b3df29e2
Natalie Hummel <natoms225@gmail.com>
2020-05-11 14:19:25 -0500

    added firstRedcuer

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ import App from './components/App/App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 
+// initial state
+const firstReducerInitialState = [];
+
 // put your reducer here!
+const firstReducer = (state = firstReducerInitialState, action) => {
+    console.log('in firstReducer, Action:', action);
+    return state;
+}
 
 // use reducer in store
-const storeInstance = createStore(  );
+const storeInstance = createStore(firstReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
