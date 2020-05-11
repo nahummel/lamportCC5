93f8eddf8afba3a3bcc4027a81df08187aad41dd
Natalie Hummel <natoms225@gmail.com>
2020-05-11 15:16:19 -0500

    Passengers are displayed

--- src/components/Passengers/Passengers.js
+++ src/components/Passengers/Passengers.js
@@ -4,15 +4,46 @@ import React, { Component } from 'react';
 // INPUT SHOULD COLLECT INFO, BUTTON SHOULD ADD THEM TO THE LIST
 
 class Passengers extends Component {
+  state = {
+    passenger: '',
+    passengers: []
+  }
+
+  componentDidMount(){
+    console.log(this.state.passengers)
+  }
+
+  clickHandler = () => {
+    console.log('in clickHandler for passengers')
+    console.log(this.state.passenger);
+    this.setState({
+      passengers: [...this.state.passengers, this.state.passenger]
+    })
+    console.log(this.state.passengers)
+  }
+
+  handleChange = (event, property) => {
+    console.log('in handleChange', property, event.target.value );
+    this.setState({
+      [property]: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Passengers</h2>
 
-        <input type="text" name="name" placeholder="Enter Name" />
-        <button>Add Passenger</button>
+        <input type="text" name="name" placeholder="Enter Name" onChange={(event) => this.handleChange(event, 'passenger')}/>
+        <button onClick = {this.clickHandler}>Add Passenger</button>
 
-        <ul>PASSENGER LIST: GOES HERE</ul>
+        <ul>PASSENGER LIST: 
+          {this.state.passengers.map((passenger) => {
+            return(
+              <li>{passenger}</li>
+            )
+          })}
+        </ul>
       
       </div>
     )
